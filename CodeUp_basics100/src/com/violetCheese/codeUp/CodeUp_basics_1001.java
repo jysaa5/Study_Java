7309aa47701d015ba93b8bc5814709b14523428d
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-02-29 00:58:08 +0900

    Create CodeUp_basics_1001.java

---
+++ CodeUp_basics100/src/com/violetCheese/codeUp/CodeUp_basics_1001.java
@@ -0,0 +1,14 @@
+//패키지명
+package com.violetCheese.codeUp;
+
+//클래스
+public class CodeUp_basics_1001 {
+	
+	//메인 메서드 = 클래스에서 실행되는 메서드(함수)
+	public static void main(String[] args) {
+		
+		System.out.print("Hello");
+	
+	}//End of main
+	
+}//End of CodeUp_basics_1001
