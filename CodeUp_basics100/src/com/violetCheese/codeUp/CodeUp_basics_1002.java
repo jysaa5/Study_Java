ae956d29206d472c7bbc5956d44cb8c8fea74a04
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-02-29 01:21:27 +0900

    Update CodeUp_basics_1002.java

--- CodeUp_basics100/src/com/violetCheese/codeUp/CodeUp_basics_1002.java
+++ CodeUp_basics100/src/com/violetCheese/codeUp/CodeUp_basics_1002.java
@@ -4,7 +4,7 @@ package com.violetCheese.codeUp;
 //클래스
 public class CodeUp_basics_1002 {
 	
-	//메인 메서드 = 클래스에서 실행되는 메서드(함수)
+	//메인 메서드
 	public static void main(String args[]) {
 		
 		System.out.print("Hello World");
