a1b52dab78a8f8012bda34d85208fe5fa4664cda
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-03-02 01:14:13 +0900

    2020.03.02_codeUp

---
+++ CodeUp_basics100/src/com/violetCheese/codeUp/CodeUp_basics_1053.java
@@ -0,0 +1,21 @@
+package com.violetCheese.codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp_basics_1053 {
+	
+	//메인 메서드
+	public static void main(String[] args) {
+		
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        if (str.equals("1")){
+            System.out.print(0);
+        }else if (str.equals("0")){
+            System.out.print(1);
+        }
+        sc.close();
+        
+	}//main 메서드 끝
+	
+}//CodeUp_basics_1053 클래스 끝
