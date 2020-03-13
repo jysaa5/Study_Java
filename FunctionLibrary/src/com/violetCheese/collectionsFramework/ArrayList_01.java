5fe2af90fa90a617f8c7a1b1ee51e5dda4e6acfe
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-03-13 20:24:43 +0900

    2020.03.13_Study Object class

---
+++ FunctionLibrary/src/com/violetCheese/collectionsFramework/ArrayList_01.java
@@ -0,0 +1,17 @@
+package com.violetCheese.collectionsFramework;
+
+import java.util.ArrayList;
+
+public class ArrayList_01 {
+
+	
+	public static void main(String[] args) {
+	    ArrayList<Object> list = new ArrayList<Object>();
+	    System.out.println(list.size());
+
+		
+	}
+	
+	
+	
+}
