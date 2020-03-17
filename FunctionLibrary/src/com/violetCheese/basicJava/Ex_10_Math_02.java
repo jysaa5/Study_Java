abecfdb66fe34b46fb9c01328bdab991c445ec29
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-03-17 23:47:15 +0900

    Study Java

---
+++ FunctionLibrary/src/com/violetCheese/basicJava/Ex_10_Math_02.java
@@ -0,0 +1,15 @@
+package com.violetCheese.basicJava;
+
+public class Ex_10_Math_02 {
+	
+	public static void main(String[] args) {
+		
+		//½Ç¼ö
+		System.out.println("2ÀÇ 10½Â = " + Math.pow(2, 10));
+		//½Ç¼ö
+		System.out.println("16ÀÇ 1/2½Â = : " + Math.pow(16, 0.5));
+		//½Ç¼ö
+		System.out.println("log200 = " + Math.log10(200));
+
+	}
+}
