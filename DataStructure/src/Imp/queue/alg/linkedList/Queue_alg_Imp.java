71f6f7c6c848db0e9057c8b02dbbd532bb931ee1
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-04-22 23:07:00 +0900

    Update Queue_alg_Imp.java

--- DataStructure/src/Imp/queue/alg/linkedList/Queue_alg_Imp.java
+++ DataStructure/src/Imp/queue/alg/linkedList/Queue_alg_Imp.java
@@ -6,13 +6,18 @@ import java.util.LinkedList;
 // 있는 순서로 자동으로 분양될 동물에 정해지는 클래스를 구현하시오.
 // 단, 자바에서 제공하는 LinkedList로 구현하시오.
 
-//
+// LinkedList로 Queue로 구현
+// 동물 타입 선언
 enum AnimalType {
+	
 	DOG, CAT
+
 }
 
 //공통 항목은 추상 클래스에서 다룬다.
+// Animal: 추상 클래스
 abstract class Animal {
+
 	AnimalType type;
 	String name;
 	int order;
@@ -23,37 +28,61 @@ abstract class Animal {
 		this.name = name;
 	}
 
+	// setOrder 메서드: 순서 설정
 	void setOrder(int order) {
+		
 		this.order = order;
-	}
+	
+	}//setOrder 메서드 종료
 
+	
+	// getOrder 메서드: 순서 보기
 	int getOrder() {
+	
 		return order;
-	}
+	
+	}// getOrder 메서드 종료
 
+	
+	// info 메서드
 	String info() {
+		
 		return order + ") type: " + type + ", name: " + name;
-	}
-}
+	
+	}// info 메서드 종료
+	
+}// Animal 추상 클래스 종료
 
+
+// Dog 클래스 
 class Dog extends Animal {
 
 	// 생성자
 	Dog(String name) {
+		
 		super(AnimalType.DOG, name);
+	
 	}
 
-}
+}// Dog 클래스 종료
 
+
+// Cat 클래스 
 class Cat extends Animal {
+	
 	// 생성자
 	Cat(String name) {
+	
 		super(AnimalType.CAT, name);
+	
 	}
-}
+	
+}// Cat 클래스 종료
+
 
 //동물 분양소
 class AnimalShelter {
+
 	LinkedList<Dog> dogs = new LinkedList<Dog>();
 	LinkedList<Cat> cats = new LinkedList<Cat>();
 
@@ -62,39 +91,72 @@ class AnimalShelter {
 
 	// 생성자
 	AnimalShelter() {
+	
 		order = 1;
+	
 	}
 
+	// enqueue 메서드: 큐에 데이터 추가하는 기능
 	void enqueue(Animal animal) {
+		//순서 설정
 		animal.setOrder(order);
+		
+		//순서 증가
 		order++;
 
+		//개 일 때
 		if (animal.type == AnimalType.DOG) {
+	
+			//개 연결리스트에 데이터 추가
 			dogs.addLast((Dog) animal);
+		
+		//고양이 일 때
 		} else if (animal.type == AnimalType.CAT) {
+		
+			//고양이 연결리스트에 데이터 추가
 			cats.addLast((Cat) animal);
+	
 		}
-	}
+		
+	}// enqueue 메서드 종료
 
+	
+	// dequeueDog 메서드: 개 큐에서 데이터 꺼내고 삭제하는 기능
 	Animal dequeueDog() {
+	
 		return dogs.poll();
-	}
+	
+	}// dequeueDog 메서드 종료
 
+	
+	// dequeueCat 메서드: 고양이 큐에서 데이터 꺼내고 삭제하는 기능 
 	Animal dequeueCat() {
+		
 		return cats.poll();
-	}
+	
+	}// dequeueCat 메서드 종료
 
+	
+	// dequeue 메서드: 고양이나 개 중에서 순서대로 아무거나 꺼내는 기능
 	Animal dequeue() {
+		
+		// 개, 고양이가 모두 다 없을 때.
 		if (dogs.size() == 0 && cats.size() == 0) {
+		
 			return null;
+		
+		// 개만 없을 때
 		} else if (dogs.size() == 0) {
 
 			return cats.poll();
 
+		// 고양이만 없을 때
 		} else if (cats.size() == 0) {
+			
 			return dogs.poll();
 		}
 		
+		//둘 다 있을 때 스택 위에 있는 것을 꺼내 본다.
 		Animal dog = dogs.peek();
 		Animal cat = cats.peek();
 
@@ -104,9 +166,11 @@ class AnimalShelter {
 		}else {
 			return dogs.poll();
 		}
-	}
+		
+	}// dequeue 메서드 종료
 
-}
+}//AnimalShelter 클래스 종료
+
 
 //Queue_alg_Imp 클래스
 public class Queue_alg_Imp {
