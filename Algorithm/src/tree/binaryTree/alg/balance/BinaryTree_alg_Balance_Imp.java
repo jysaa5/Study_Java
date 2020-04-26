1ecdaacbbec6dd76fed0264af2e2dfe162270036
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-04-26 22:09:17 +0900

    2020.04.26_Update code

--- Algorithm/src/tree/binaryTree/alg/balance/BinaryTree_alg_Balance_Imp.java
+++ Algorithm/src/tree/binaryTree/alg/balance/BinaryTree_alg_Balance_Imp.java
@@ -1,4 +1,4 @@
-package tree.alg.balance;
+package tree.binaryTree.alg.balance;
 
 // 문제: 주어진 이진 트리의 Balance가 맞는지 확인하는 함수를 구현하시오.
 // (여기서 Balance가 맞다는 의미는 어떤 노드의 양쪽 서브 트리의 길이가 1초과 차이가 나지 않는 것을 뜻함)
@@ -51,7 +51,7 @@ package tree.alg.balance;
 
 // Solution: 노드들 하나씩 돌면서 자식 노드를 재귀 호출하고, 서브 트리의 길이를 측정하는 함수를 만들어서 양쪽 서브 
 // 트리의 길이를 비교하다가 두 개의 서브 트리 길이가 1초과가 발생하면 false를 반환한다.
-
+// 시간 복잡도: O(n log n)
 class Tree{
 	class Node{
 		int data;
@@ -69,6 +69,8 @@ class Tree{
 	// 생성자
 	Tree(int size){
 		root = makeBST(0, size-1);
+		// unbalanced 만들기
+		root.right.right.right.right = new Node(10);
 	}
 	
 	// 이진트리를 만드는 메서드
@@ -87,30 +89,41 @@ class Tree{
 
 	
 	// isBalanced 메서드
+	// 노드의 주소를 인자로 받는다.
 	boolean isBalanced(Node root) {
 	
+		// 마지막 노드를 만나면 true를 반환
 		if(root == null) {
 			return true;
 		}
 		
+		// 서브 트리 길이 차이 비교
 		int heightDiff = getHeight(root.left) - getHeight(root.right);
 		
+		// 1초과 차이가 날때
 		if(Math.abs(heightDiff)>1) {
 			return false;
+		
 		}else {
+		
+			// 왼쪽과 오른쪽이 모두 balanced 일 때 true 반환
 			return isBalanced(root.left) && isBalanced(root.right);
+		
 		}
 		
 	}// isBalanced 메서드 종료
 	
 	
 	// getHeight 메서드 
+	// 노드의 주소를 인자로 받는다.
 	int getHeight(Node root) {
 		
+		// 마지막 노드를 만날 때.
 		if(root == null) {
 			return -1;
 		}
 		
+		// 반환할 때마다 1을 더한다.
 		return Math.max(getHeight(root.left), getHeight(root.right)) +1;
 	}// getHeight 메서드 종료
 	
@@ -120,12 +133,13 @@ class Tree{
 
 
 // Tree_Balance_Imp 클래스
-public class Tree_Balance_Imp {
+public class BinaryTree_alg_Balance_Imp {
 	
 	// main 메서드
 	public static void main(String[] args) {
 		
 		Tree t = new Tree(10);
+		// balanced 한 경우
 		System.out.println(t.isBalanced(t.root));
 		
 		
