05ea26eec16774d64bb718972db29cca998db910
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-04-30 20:40:26 +0900

    Create BinaryTree_alg_find_commonParent_sol_1.java

---
+++ Algorithm/src/tree/binaryTree/alg/find/common/parent_sol_1/BinaryTree_alg_find_commonParent_sol_1.java
@@ -0,0 +1,9 @@
+package tree.binaryTree.alg.find.common.parent_sol_1;
+
+// 문제: 이진 트리에서 주어진 두 개의 노드의 첫 번째 공통된 부모 노드를 찾으시오. (단, 다른 자료구조는 사용 금지)
+// Solution 1: 길이 맞추서 풀기
+
+
+public class BinaryTree_alg_find_commonParent_sol_1 {
+
+}
