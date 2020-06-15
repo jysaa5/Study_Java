c2b8b5f8ad5959e6586291fc29fc14feb62bb2d7
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-06-15 21:54:56 +0900

    2020.06.15_Study Calendar Class

--- Java_Standard_Procedure/src/chapter10/date/time/and/formatting/Calendar_01.java
+++ Java_Standard_Procedure/src/chapter10/date/time/and/formatting/Calendar_01.java
@@ -5,6 +5,8 @@ import java.util.Calendar;
 public class Calendar_01 {
 	
 	public static void main(String[] args) {
+		
+		// 기본적으로 현재날짜와 시간으로 설정된다.
 		Calendar today = Calendar.getInstance();
 		
 		System.out.println("이 해의 년도: "+ today.get(Calendar.YEAR));
@@ -12,9 +14,12 @@ public class Calendar_01 {
 		System.out.println("이 해의 몇 째 주: "+ today.get(Calendar.WEEK_OF_YEAR));
 		System.out.println("이 달의 몇 째 주: "+ today.get(Calendar.WEEK_OF_MONTH));
 		
+		// DATE와 DAY_OF_MONTH는 같다.
 		System.out.println("이 달의 몇 일:"+ today.get(Calendar.DATE));
 		System.out.println("이 달의 몇 일:"+ today.get(Calendar.DAY_OF_MONTH));
 		System.out.println("이 해의 몇 일"+ today.get(Calendar.DAY_OF_YEAR));
+		
+		// 1:일요일, 2:월요일, ... 7: 토요일
 		System.out.println("요일(1~7, 1:일요일):" + today.get(Calendar.DAY_OF_WEEK));
 		
 		System.out.println("이 달의 몇 째 요일:"+today.get(Calendar.DAY_OF_WEEK_IN_MONTH));
@@ -25,6 +30,7 @@ public class Calendar_01 {
 		System.out.println("초(0~59)"+today.get(Calendar.SECOND));
 		System.out.println("1000분의 1초(0~999): "+ today.get(Calendar.MILLISECOND));
 		
+		// 천분의 1초를 시간으로 표시하기 위해 3600000으로 나누었다.(1시간 = 60 * 60초)
 		System.out.println("TimeZone(-12 ~ +12): "+(today.get(Calendar.ZONE_OFFSET)/(60*60*1000)));
 		System.out.println("이달의 마지막 날: "+today.getActualMaximum(Calendar.DATE));
 		
