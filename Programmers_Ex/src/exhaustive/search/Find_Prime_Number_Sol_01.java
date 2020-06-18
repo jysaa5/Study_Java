37b2d8c9e2e6591472769548d5a149115bc94cc9
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-06-18 21:06:14 +0900

    2020.06.18_Update

--- Programmers_Ex/src/exhaustive/search/Find_Prime_Number_Sol_01.java
+++ Programmers_Ex/src/exhaustive/search/Find_Prime_Number_Sol_01.java
@@ -6,36 +6,30 @@ import java.util.Arrays;
 문제 설명
 한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 
 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
-
 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때,
 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
-
 제한사항
 numbers는 길이 1 이상 7 이하인 문자열입니다.
 numbers는 0~9까지 숫자만으로 이루어져 있습니다.
 013은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
-
 입출력 예
 numbers | return
 17      | 3
 011	    | 2
-
 입출력 예 설명
 예제 #1
 [1, 7]으로는 소수 [7, 17, 71]를 만들 수 있습니다.
-
 예제 #2
 [0, 1, 1]으로는 소수 [11, 101]를 만들 수 있습니다.
-
 11과 011은 같은 숫자로 취급합니다.
 */
 
-
+// 솔루션
 /*
 솔루션 두 가지 방법
+
 첫 번째: 숫자 조각으로 만들 수 있는 모든 숫자를 만들고 그 숫자들 중 소수의 개수를 세는 방법
-두 번째: 숫자 조각으로 구할 수 있는 최대 수까지 소수를 모두 구한 뒤 모든 소수를 검사하며 해당 소수가 숫자 조각들로 이루어질 수
-있는 지 확인하는 방법
+두 번째: 숫자 조각으로 구할 수 있는 최대 수까지 소수를 모두 구한 뒤 모든 소수를 검사하며 해당 소수가 숫자 조각들로 이루어질 수 있는 지 확인하는 방법
 
 -> 두 번째 방법
 1) 숫자 조각들로 구할 수 있는 가장 큰 값을 구한다.
@@ -46,101 +40,138 @@ numbers | return
 소수에서 사용된 숫자 만큼 숫자의 개수를 줄여준다.
 소수에서 사용된 수가 사용할 수 있는 수보다 많다면 숫자 조각들로 만들 수 없는 소수이다.
 
-
 ex) 주어진 수: 1, 2, 3
        숫자    : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 사용가능 개수: 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 
+
 */
 
 // 
 class Find_Prime_Number_Solution_01 {
-	
+
 	
 	public int solution(String numbers) {
-	    int maxNum = makeMaxNum(numbers.toCharArray());
-	    boolean[] primeNum = findPrimeNum(maxNum);
+		
+		// 주어진 숫자로 만들 수 있는 최대 수
+		int maxNum = makeMaxNum(numbers.toCharArray());
+		
+		boolean[] primeNum = findPrimeNum(maxNum);
 
-	    return findMakePossiblePrimeNum(primeNum, maxNum);
+		return findMakePossiblePrimeNum(primeNum, maxNum);
 	}
+
+	
 	
 	// 주어진 숫자중에서 만들 수 있는 최대 수
 	// 주어진 숫자 조각들에서 구하 수 있는 가장 큰 값을 int형으로 반환한다.
 	private int makeMaxNum(char[] inputNum) {
-		
+
 		// 오름차순 정렬
-	    Arrays.sort(inputNum);
-	    int len = inputNum.length;
+		Arrays.sort(inputNum);
+		int len = inputNum.length;
 		// 내림차순으로 다시 재정렬
-	    for (int i = 0; i < len/2; i++) {
-	        char temp = inputNum[i];
-	        inputNum[i] = inputNum[len - 1 - i];
-	        inputNum[len - 1 - i] = temp;
-	    }
-	 // 문자배열을 문자열로 만들고 정수로 변환 후, 반환
-	    return Integer.parseInt(new String(inputNum));
+		for (int i = 0; i < len / 2; i++) {
+			
+			char temp = inputNum[i];
+			inputNum[i] = inputNum[len - 1 - i];
+			inputNum[len - 1 - i] = temp;
+	
+		}
+		
+		// 문자배열을 문자열로 만들고 정수로 변환 후, 반환
+		return Integer.parseInt(new String(inputNum));
 	}
+
 	
 	
-	// 최대 수 안에서 소수를 찾는 메서드 
+	// 최대 수 안에서 소수를 찾는 메서드
 	// 주어진 가장 큰 값까지의 소수를 에라토스테네스의 체를 이용하여 구한다.
 	private boolean[] findPrimeNum(int maxNum) {
-	    boolean[] result = new boolean[maxNum + 1];
-	    for (int i = 2; i < Math.sqrt(maxNum); i++) {
-	        if (!result[i]) {
-	            for (int j = i * 2; j <= maxNum; j += i) {
-	                result[j] = true;
-	            }
-	        }
-	    }
-	    return result;
+		
+		boolean[] result = new boolean[maxNum + 1];
+		
+		for (int i = 2; i < Math.sqrt(maxNum); i++) {
+			
+			if (!result[i]) {
+				
+				for (int j = i * 2; j <= maxNum; j += i) {
+					
+					result[j] = true;
+				}
+			}
+		}
+		return result;
 	}
-	
-	
 
+	
+	
 	// 주어진 숫자조각으로 만들 수 있는 소수들의 개수를 구하여 반환하는 함수이다.
 	private int findMakePossiblePrimeNum(boolean[] primeNum, int maxNum) {
-	    int possiblePrimeNumCount = 0;
+		
+		int possiblePrimeNumCount = 0;
 
-	    for (int primeNumIndex = 2; primeNumIndex <= maxNum; primeNumIndex++) {
-	        if (!primeNum[primeNumIndex] && isPossible(maxNum, primeNumIndex)) {
-	            possiblePrimeNumCount++;
-	        }
-	    }
-	    return possiblePrimeNumCount;
+		for (int primeNumIndex = 2; primeNumIndex <= maxNum; primeNumIndex++) {
+			
+			if (!primeNum[primeNumIndex] && isPossible(maxNum, primeNumIndex)) {
+			
+				possiblePrimeNumCount++;
+		
+			}
+			
+		}
+		return possiblePrimeNumCount;
 	}
 
 	
-	// 해당 소수가 주어진 숫자 조각으로 만들어질 수 있는지 여부를 판단하는 함수이다. 
+	
+	// 해당 소수가 주어진 숫자 조각으로 만들어질 수 있는지 여부를 판단하는 함수이다.
 	private boolean isPossible(int maxNum, int primeNum) {
-	    int[] numCount = countAvailableNums(maxNum);
+		
+		int[] numCount = countAvailableNums(maxNum);
 
-	    while(primeNum != 0) {
-	        if (numCount[primeNum % 10] <= 0) return false;
-	        numCount[primeNum % 10]--;
-	        primeNum /= 10;
-	    }
+		while (primeNum != 0) {
+			
+			if (numCount[primeNum % 10] <= 0) {
+				return false;
+			}
+			
+			numCount[primeNum % 10]--;
+			
+			primeNum /= 10;
+		}
 
-	    return true;
+		return true;
 	}
+
+	
 	
 	// 주어진 숫자조각에서 사용할 수 있는 0~9의 개수를 세는 함수이다.
 	private int[] countAvailableNums(int maxNum) {
-	    char[] str = String.valueOf(maxNum).toCharArray();
-	    int[] numCount = new int[10];
-	    for (int i = 0; i < str.length; i++) {
-	        numCount[str[i] - '0']++;
-	    }
-	    return numCount;
+		
+		char[] str = String.valueOf(maxNum).toCharArray();
+		
+		int[] numCount = new int[10];
+		
+		for (int i = 0; i < str.length; i++) {
+		
+			numCount[str[i] - '0']++;
+		
+		}
+		
+		return numCount;
 	}
 }
 
+
+
 public class Find_Prime_Number_Sol_01 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Find_Prime_Number_Solution_01 sol = new Find_Prime_Number_Solution_01();
-		
+
 		String numbers = "17";
+
 		System.out.println(sol.solution(numbers));
 	}
 
--- Programmers_Ex/src/exhaustive/search/Find_Prime_Number_Sol_02.java
+++ Programmers_Ex/src/exhaustive/search/Find_Prime_Number_Sol_02.java
@@ -2,12 +2,12 @@ package exhaustive.search;
 
 import java.util.HashSet;
 
-//문제: 소수 찾기
+// 문제: 소수 찾기
 /*
 문제 설명
+
 한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 
 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
-
 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때,
 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 
@@ -24,27 +24,36 @@ numbers | return
 입출력 예 설명
 예제 #1
 [1, 7]으로는 소수 [7, 17, 71]를 만들 수 있습니다.
-
 예제 #2
 [0, 1, 1]으로는 소수 [11, 101]를 만들 수 있습니다.
-
 11과 011은 같은 숫자로 취급합니다.
 */
+
+// 솔루션: 재귀를 이용함.
 
 class Find_Prime_Number_Solution_02 {
 
 	public int solution(String numbers) {
 		
-		HashSet<Integer> set = new HashSet<>();
+		// 해시집합 선언 -> 중복된 것을 만들지 않기 위해서.
+		HashSet<Integer> set = new HashSet<Integer>();
+		
 		permutation("", numbers, set);
 		
 		int count = 0;
 		
+		// 해시집합에서 숫자들을 삭제하면서 소수이면 count를 증가 시킨다.
 		while (set.iterator().hasNext()) {
+		
 			int a = set.iterator().next();
+			
 			set.remove(a);
-			if (a == 2)
+			
+			if (a == 2) {
 				count++;
+			}
+			
+			// 2의 배수가 아니면서 홀수로도 나누었을 때 아니어야 소수가 된다.
 			if (a % 2 != 0 && isPrime(a)) {
 				count++;
 			}
@@ -53,35 +62,50 @@ class Find_Prime_Number_Solution_02 {
 	}
 
 	
+	// 소수인지 아닌지 판별 
 	public boolean isPrime(int n) {
 	
 		if (n == 0 || n == 1) {
 			return false;
 		}
 		
+		// 수의 제곱근까지 홀수를 찾아서 해당 수를 나누는 것을 반복한다.
 		for (int i = 3; i <= (int) Math.sqrt(n); i += 2) {
+			
 			if (n % i == 0) {
 				return false;
 			}
+			
 		}
+		
 		return true;
 	}
 
+	// prefix: 접두사 -> "" 공백 없는 문자 한 개씩을 만들어냄
+	// str: 주어진 숫자를 string으로 받는다. 
+	// permutation: 순열 
 	public void permutation(String prefix, String str, HashSet<Integer> set) {
 		
 		int n = str.length();
 		// if (n == 0) System.out.println(prefix);
 		
+		// 공백이 아니면 해시집합에 저장
 		if (!prefix.equals("")) {
+			
+			// 문자의 접두를 정수로 받아서 저장
 			set.add(Integer.valueOf(prefix));
+		
 		}
 
+		// 재귀함수 호출
 		for (int i = 0; i < n; i++) {
+			// 문자열에서 한 개씩 떼어내기 위해서 재귀를 부른다.
 			permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n), set);
 		}
 	}
 
 }
+
 
 public class Find_Prime_Number_Sol_02 {
 
@@ -90,6 +114,7 @@ public class Find_Prime_Number_Sol_02 {
 		Find_Prime_Number_Solution_02 sol = new Find_Prime_Number_Solution_02();
 
 		String numbers = "17";
+		
 		System.out.println(sol.solution(numbers));
 	}
 
