fb5438f3e6a1b5474a1bc8554422ac27c50d19d0
JooYeon Kim <57422057+jysaa5@users.noreply.github.com>
2020-08-13 11:48:45 +0900

    2020.08.13_update

---
+++ Java_Standard_Procedure/src/chapter04/conditional/repetitive/statement/Break_01.java
@@ -0,0 +1,22 @@
+package chapter04.conditional.repetitive.statement;
+
+// p.175
+public class Break_01 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		
+		while(true) {
+			if(sum>100) {
+				break;
+			}
+			++i;
+			sum += i;
+			
+		}// end of while
+		
+		System.out.println("i="+i);
+		System.out.println("sum="+sum);
+		
+	}
+}
---
+++ Java_Standard_Procedure/src/chapter11/collections/framework/Comparator_02.java
@@ -0,0 +1,46 @@
+package chapter11.collections.framework;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// p.629
+public class Comparator_02 {
+	
+	public static void main(String[] args) {
+		
+		String[] strArr = {"cat", "Dog", "lion", "tiger"};
+		
+		// String의 Comparable 구현에 의한 정렬
+		Arrays.sort(strArr); 
+		System.out.println("strArr = "+Arrays.toString(strArr));
+		
+		// 대소문자 구분 안함.
+		Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER);
+		System.out.println("strArr = "+Arrays.toString(strArr));
+		
+		// 역순 정렬
+		Arrays.sort(strArr, new Descending());
+		System.out.println("strArr= "+Arrays.toString(strArr));
+		
+	}
+
+}
+
+class Descending implements Comparator{
+	
+	public int compare(Object o1, Object o2) {
+		
+		if(o1 instanceof Comparable && o2 instanceof Comparable) {
+			
+			Comparable c1 = (Comparable)o1;
+			Comparable c2 = (Comparable)o2;
+			
+			// -1을 곱해서 기본 정렬 방식의 역으로 변경한다.
+			// 또는 c2.compareTo(c1)와 같이 순서를 바꿔도 된다.
+			return c1.compareTo(c2)*-1;
+		}
+		
+		return -1;
+		
+	}
+}
