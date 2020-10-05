e88a5e05a17a19aad3151ed26b4741bdc2abcf6d
jysaa5 <jysaa5@naver.com>
2020-10-05 20:50:46 +0900

    Solve problem

---
+++ BaekJoon_Ex/src/input/and/output/arithmetic/operations/A_Minus_B_Ex.java
@@ -0,0 +1,18 @@
+package input.and.output.arithmetic.operations;
+/*
+public class Main{
+public static void main(String[] args){
+}
+}
+*/
+// ¹®Á¦: 1001 (A-B)
+import java.util.Scanner;
+public class A_Minus_B_Ex {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print(a-b);
+	}
+
+}
