5ddca8681ca21e44c16910ef03692884112b15fc
jysaa5 <jysaa5@naver.com>
2020-10-06 22:31:32 +0900

    Solve problem

---
+++ BaekJoon_Ex/src/input/and/output/arithmetic/operations/A_Multiple_B_Ex.java
@@ -0,0 +1,17 @@
+package input.and.output.arithmetic.operations;
+/*
+public class Main{
+public static void main(String[] args){
+}
+}
+*/
+// ¹®Á¦: 10998 (A*B)
+import java.util.Scanner;
+public class A_Multiple_B_Ex {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print(a*b);
+	}
+}
