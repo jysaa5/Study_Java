af0af1d8a82991f61cd8c768d27b82397c72e7a0
jysaa5 <jysaa5@naver.com>
2020-10-08 00:05:11 +0900

    update

--- BaekJoon_Ex/src/input/and/output/arithmetic/operations/Multiple_Ex.java
+++ BaekJoon_Ex/src/input/and/output/arithmetic/operations/Multiple_Ex.java
@@ -12,46 +12,56 @@ import java.util.Scanner;
 public class Multiple_Ex {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		String str1 = sc.nextLine().trim();
+		String str2 = sc.nextLine().trim();
 		int[] input1 = new int[3];
 		int[] input2 = new int[3];
 
 		for (int i = 0; i < 3; i++) {
-			input1[i] = sc.nextInt();
+			input1[i] = Integer.parseInt("" + str1.charAt(i));
 		}
 
 		for (int j = 0; j < 3; j++) {
-			input2[j] = sc.nextInt();
+			input2[j] = Integer.parseInt("" + str2.charAt(j));
 		}
 
-		int[] result = new int[3];
+		int[] result = new int[4];
 
 		int carry = 0;
 		int num = 0;
 
-			for (int l = 2; l > -1; l--) {
-				StringBuilder sb = new StringBuilder();
-				for (int n = 2; n > -1; n--) {
-					if (n == 2) {
-						carry = (input1[n] * input2[l]) / 10;
-						num = (input1[n] * input2[l]) % 10;
-					}else if(n == 1){
-						num = carry + (input1[n] * input2[l]);
-						carry = num/10;
-						num = num%10;
-					}else {
-						num = carry + (input1[n] * input2[l]);
-					}
-					
-					sb.append(num);
+		for (int l = 2; l > -1; l--) {
+			StringBuilder sb = new StringBuilder();
+			for (int n = 2; n > -1; n--) {
+				if (n == 2) {
+					carry = (input1[n] * input2[l]) / 10;
+					num = (input1[n] * input2[l]) % 10;
+				} else if (n == 1) {
+					num = carry + (input1[n] * input2[l]);
+					carry = num / 10;
+					num = num % 10;
+				} else {
+					num = carry + (input1[n] * input2[l]);
 				}
-				result[l] = Integer.parseInt(sb.toString());
+
+				sb.insert(0, num);
 			}
-			
-			for(int n : result) {
-				System.out.println(n);
+			result[2 - l] = Integer.parseInt(sb.toString());
+			if (2 - l == 0) {
+				result[3] += result[2 - l];
 			}
-		
+			if (2 - l == 1) {
+				result[3] += result[2 - l] * 10;
+			}
+			if (2 - l == 2) {
+				result[3] += result[2 - l] * 100;
+			}
+		}
+
+		for (int n : result) {
+			System.out.println(n);
+		}
+
 	}
 
 }
