bfc684e8c0eb67cb7e0cc1c884edb175a3ccb223
jysaa5 <jysaa5@naver.com>
2020-11-04 23:21:49 +0900

    Solve Problem

---
+++ BaekJoon_Ex/src/character/string/Croatian_Alphabet_Ex.java
@@ -0,0 +1,24 @@
+package character.string;
+
+/*
+public class Main{
+public static void main(String[] args){
+}
+}
+*/
+// 문제: 2941 (크로아티아 알파벳)
+import java.util.Scanner;
+
+public class Croatian_Alphabet_Ex {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] ca = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+		String str = sc.nextLine();
+		int result = 0;
+
+		for (int i = 0; i < ca.length; i++) {
+			str = str.replaceAll(ca[i], "1");
+		}
+		System.out.println(str.length());
+	}
+}
