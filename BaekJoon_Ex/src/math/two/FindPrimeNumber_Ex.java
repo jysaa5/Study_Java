065e8d111beb3e0f25be0915f282dbc30a6f457a
jysaa5 <jysaa5@naver.com>
2020-12-02 22:37:40 +0900

    Create FindPrimeNumber_Ex.java

---
+++ BaekJoon_Ex/src/math/two/FindPrimeNumber_Ex.java
@@ -0,0 +1,13 @@
+package math.two;
+/*
+public class Main{
+public static void main(String[] args){
+}
+}
+*/
+// 문제: 1978 (소수찾기)
+public class FindPrimeNumber_Ex {
+	public static void main(String[] args) {
+		
+	}
+}
