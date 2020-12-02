f8856051224b0415985713d937ae2ed0d63f38d4
jysaa5 <jysaa5@naver.com>
2020-12-02 23:25:11 +0900

    Update FindPrimeNumber_Ex.java

--- BaekJoon_Ex/src/math/two/FindPrimeNumber_Ex.java
+++ BaekJoon_Ex/src/math/two/FindPrimeNumber_Ex.java
@@ -1,4 +1,5 @@
 package math.two;
+
 /*
 public class Main{
 public static void main(String[] args){
@@ -6,8 +7,32 @@ public static void main(String[] args){
 }
 */
 // 문제: 1978 (소수찾기)
+import java.util.Scanner;
+
 public class FindPrimeNumber_Ex {
+	static boolean isPrime(int n) {
+		if(n<=1) {
+			return false;
+		}
+		if((n & 1) == 0) {
+			return (n==2);
+		}
+		for(int i=3; i*i<=n; i++) {
+			if(n%i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		int cnt = 0;
+		for (int i = 0; i < number; i++) {
+			int n = sc.nextInt();
+			cnt += (isPrime(n)) ? 1 : 0; 
+		}
+		System.out.println(cnt);
+		sc.close();
 	}
 }
