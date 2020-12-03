eea4e2cee41621c039cf0a42735b5939dbef79fb
jysaa5 <jysaa5@naver.com>
2020-12-03 23:19:07 +0900

    Create PrimeNumber_Ex.java

---
+++ BaekJoon_Ex/src/math/two/PrimeNumber_Ex.java
@@ -0,0 +1,14 @@
+package math.two;
+/*
+public class Main{
+public static void main(String[] args){
+}
+}
+*/
+// 문제: 2581 (소수)
+import java.util.Scanner;
+public class PrimeNumber_Ex {
+	public static void main(String[] args) {
+		
+	}
+}
