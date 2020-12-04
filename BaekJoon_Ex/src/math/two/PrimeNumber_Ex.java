799a6ee4f93560c690b5c29a4c3faf736e7d9e95
jysaa5 <jysaa5@naver.com>
2020-12-04 15:00:40 +0900

    Update PrimeNumber_Ex.java

--- BaekJoon_Ex/src/math/two/PrimeNumber_Ex.java
+++ BaekJoon_Ex/src/math/two/PrimeNumber_Ex.java
@@ -8,7 +8,52 @@ public static void main(String[] args){
 // 문제: 2581 (소수)
 import java.util.Scanner;
 public class PrimeNumber_Ex {
+	public static boolean prime[];
+	
 	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int M = in.nextInt();
+		int N = in.nextInt();
+		
+		prime = new boolean[N + 1];	// 배열 생성 
+		get_prime();
+		
+		
+		// 소수 합 및 최솟값 
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for(int i = M; i <= N; i++) {
+			if(prime[i] == false) {	// false = 소수 
+				sum += i;
+				if(min == Integer.MAX_VALUE) {	// 첫 소수가 최솟값임  
+					min = i;
+				}
+			}
+		}
+		
+		if(sum == 0) {	// 소수가 없다면 
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+		
+	}
+ 
+	
+	// 에라토스테네스 체 알고리즘 
+	public static void get_prime() {
+		prime[0] = true;
+		prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
 		
 	}
 }
